8bbb4b056e0c83d95016864e01e1fb760c233bd6
XuanSuTrum <125566435+XuanSuTrum@users.noreply.github.com>
2023-03-28 19:48:05 +0800

    Add files via upload

---
+++ EegConvdSave.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 23 20:11:13 2023
+
+@author: Administrator
+"""
+import os
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np 
+import numpy as np
+import os
+import gzip
+
+# 定义加载数据的函数，data_folder为保存gz数据的文件夹，该文件夹下有4个文件
+# 'train-labels-idx1-ubyte.gz', 'train-images-idx3-ubyte.gz',
+# 't10k-labels-idx1-ubyte.gz', 't10k-images-idx3-ubyte.gz'
+
+def load_data(data_folder):
+
+  files = [
+      'train-labels-idx1-ubyte.gz', 'train-images-idx3-ubyte.gz',
+      't10k-labels-idx1-ubyte.gz', 't10k-images-idx3-ubyte.gz'
+  ]
+
+  paths = []
+  for fname in files:
+    paths.append(os.path.join(data_folder,fname))
+
+  with gzip.open(paths[0], 'rb') as lbpath:
+    y_train = np.frombuffer(lbpath.read(), np.uint8, offset=8)
+
+  with gzip.open(paths[1], 'rb') as imgpath:
+    x_train = np.frombuffer(
+        imgpath.read(), np.uint8, offset=16).reshape(len(y_train), 28, 28)
+
+  with gzip.open(paths[2], 'rb') as lbpath:
+    y_test = np.frombuffer(lbpath.read(), np.uint8, offset=8)
+
+  with gzip.open(paths[3], 'rb') as imgpath:
+    x_test = np.frombuffer(
+        imgpath.read(), np.uint8, offset=16).reshape(len(y_test), 28, 28)
+
+  return (x_train, y_train), (x_test, y_test)
+
+(train_images, train_labels), (test_images, test_labels) = load_data('E:/数据集/datasets/')
+
+train_labels = train_labels[:1000]
+test_labels = test_labels[:1000]
+
+train_images = train_images[:1000].reshape(-1, 28 * 28) / 255.0
+test_images = test_images[:1000].reshape(-1, 28 * 28) / 255.0
+
+# Define a simple sequential model
+def create_model():
+  model = tf.keras.Sequential([
+    keras.layers.Dense(512, activation='relu', input_shape=(784,)),
+    keras.layers.Dropout(0.2),
+    keras.layers.Dense(10)
+  ])
+
+  model.compile(optimizer='adam',
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
+
+  return model
+
+# Create a basic model instance
+model = create_model()
+
+# Display the model's architecture
+model.summary()
+
+checkpoint_path = "E:/代码/脑电深度学习模型/training_1/cp.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# Create a callback that saves the model's weights
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
+
+# Train the model with the new callback
+model.fit(train_images, 
+          train_labels,  
+          epochs=10,
+          validation_data=(test_images, test_labels),
+          callbacks=[cp_callback])  # Pass callback to training
+
+# This may generate warnings related to saving the state of the optimizer.
+# These warnings (and similar warnings throughout this notebook)
+# are in place to discourage outdated usage, and can be ignored.
+
+os.listdir(checkpoint_dir)
+# Create a basic model instance
+model = create_model()
+
+# Evaluate the model
+loss, acc = model.evaluate(test_images, test_labels, verbose=2)
+print("Untrained model, accuracy: {:5.2f}%".format(100 * acc))
+
+# Loads the weights
+model.load_weights(checkpoint_path)
+
+# Re-evaluate the model
+loss, acc = model.evaluate(test_images, test_labels, verbose=2)
+print("Restored model, accuracy: {:5.2f}%".format(100 * acc))
+
